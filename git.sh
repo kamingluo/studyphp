3b8700db4ef6b7c6951cdb0c60a9446534355e53
kaming <ljm@xlbzone.com>
2019-03-25 16:23:18 +0800

    kaming提交的修改

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+git pull
 #把所有修改暂存仓库
 git add .
 
@@ -9,3 +10,32 @@ git commit -m 'kaming提交的修改'
 git push -u origin master
 
 echo "------推送成功--------"
+
+
+git commit -m '创建文件夹'
+
+#设置git信息
+#git config --global user.email "954087620@qq.com.com"
+#git config --global user.name "kaming"
+
+#新建分支
+#git branch 分支名
+
+#切换分支
+#git checkout 分支名
+
+
+#拉取远程仓库文件
+#git pull
+
+#把所有修改暂存仓库
+#git add .
+
+#提交说明
+#git commit -m 'kaming提交的修改'
+
+#把修改推送到远程仓库
+#git push -u origin master
+
+#推送上去的固定的分支的时候加上分支名就好了
+#git push -u origin 分支名
