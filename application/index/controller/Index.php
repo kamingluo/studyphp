58edf6ca8dfe8a4d6ac660d4ba2032483dd21f76
kaming <ljm@xlbzone.com>
2019-03-25 16:46:58 +0800

    kaming提交的修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,13 +11,17 @@ class Index
     {
 
     	//dump(Db::query("select * from box_gdt"));
+        //
          //加了日志就报错
     	//Log::info('查看是否存在记录');
+        //
         // $res =Db::table('box_gdt')->where('master_id',0)->select();
         // dump($res);
-        //$res1=Db::table('index_box')->select();
-        //dump($res1);
-        //dump("1234")
-         dump(Config::get());	
+        $res1=Db::table('index_box')->select();
+        dump($res1);
+        
+
+        //打印配置信息
+        //dump(Config::get());	
     }
 }
