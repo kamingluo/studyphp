d0ed1008ddc37cbac3fa9c89ffe9a08ce533e004
kaming <ljm@xlbzone.com>
2019-03-25 16:49:38 +0800

    kaming提交的修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -15,8 +15,8 @@ class Index
          //加了日志就报错
     	//Log::info('查看是否存在记录');
         //
-        // $res =Db::table('box_gdt')->where('master_id',0)->select();
-        // dump($res);
+        $res =Db::table('box_gdt')->where('master_id',0)->select();
+        dump($res);
        // $res1=Db::table('index_box')->select();
         //dump($res1);
         
