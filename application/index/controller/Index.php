c8b86fb71236778dd374b831a14ce35da77ab575
kaming <ljm@xlbzone.com>
2019-03-25 16:48:30 +0800

    kaming提交的修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -17,11 +17,11 @@ class Index
         //
         // $res =Db::table('box_gdt')->where('master_id',0)->select();
         // dump($res);
-        $res1=Db::table('index_box')->select();
-        dump($res1);
+       // $res1=Db::table('index_box')->select();
+        //dump($res1);
         
 
         //打印配置信息
-        //dump(Config::get());	
+        dump(Config::get());	
     }
 }
