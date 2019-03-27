ba735ee6c2f13fe71e736a28351fb1d5c2d6c6b6
kaming <ljm@xlbzone.com>
2019-03-27 15:17:34 +0800

    kaming提交的修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -28,10 +28,10 @@ class Index
 
     public function ceshi()
     {
-        return  "index模块下的index控制器ceshi方法777" ;
+        return  "index模块下的index控制器ceshi方法888" ;
     }
      public function test()
     {
-        return  "index模块下的index控制器test方法66666" ;
+        return  "index模块下的index控制器test方法9999" ;
     }
 }
