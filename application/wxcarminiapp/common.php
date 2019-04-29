06435a0a11f2999dc7beffdbad88f2bf30bb0c15
kaming <ljm@xlbzone.com>
2019-04-29 17:48:14 +0800

    优化拿openid接口

--- application/wxcarminiapp/common.php
+++ application/wxcarminiapp/common.php
@@ -3,28 +3,40 @@
 // | wxcarminiapp公共方法
 // +----------------------------------------------------------------------
 
-// 应用公共文件
-// 
+
+
+
+//测试方法
 function test(){
     return "test";
 
 }
 
 
+/**
+   * 调取微信接口获取openid
+   * 传入值code从小程序login API获取
+   * @return string
+*/
+function openid($wxcode){
+    $url = 'https://api.weixin.qq.com/sns/jscode2session';
+    $data['appid']=Config('appid');
+    $data['secret']= Config('secret');
+    $data['js_code']= $wxcode;
+    $data['grant_type']= 'authorization_code';
+    $wxopenid = http($url, $data, 'GET');
+    $openiddata=json_decode($wxopenid,true);
+    $rest=array_key_exists("errcode",$openiddata);//判断返回值存在errcode证明code有误
+        if($rest){ 
+            die("code错误或者过期了！");
+        }
+        else{
+        	$openid=$openiddata['openid'];
+        	return $openid;
+        }
+}
 
-// function openid($wxcode){
 
-//     // return $wxcode;
-
-//     $url = 'https://api.weixin.qq.com/sns/jscode2session';
-//     $data['appid']='wxd53f125c7e4c7d44';
-//     $data['secret']= '07f5c193225590c07c8193fd53e4b2a2';
-//     $data['js_code']= $wxcode;
-//     $data['grant_type']= 'authorization_code';
-//     $wxopenid = http($url, $data, 'GET');
-//     return $wxopenid;//去除转义
-    
-// }
 
 
 
--- application/wxcarminiapp/controller/User.php
+++ application/wxcarminiapp/controller/User.php
@@ -19,45 +19,59 @@ class User
         $data['js_code']= $wxcode;
         $data['grant_type']= 'authorization_code';
     	$wxopenid = http($url, $data, 'GET');
-    	return json_decode($wxopenid,true);//去除转义
+    	$openiddata=json_decode($wxopenid,true);
+        $rest=array_key_exists("errcode",$openiddata);//判断返回值存在errcode证明code有误
+        if($rest){ 
+            die("code错误或者过期了！");
+        }
+        else{
+            $openid=$openiddata['openid'];
+            return $openid;
+        }
     }
     
     /** 用户注册 */
     public function register(Request $request){
         $data = $request->param();//接收所有传过来的post值
         $wxcode=$data['code'];
-        $wxdata=$this->openid($wxcode);
-        $rest=array_key_exists("errcode",$wxdata);//判断返回值存在errcode证明code有误
-        if($rest){ 
-            return $wxdata;
-        }
-        else{
-            $openid=$wxdata['openid'];
-            $time =date('Y-m-d H:i:s',time());//获取当前时间
-            $dbnum =db('user')->where('openid',$openid)->find();
-            if($dbnum==null){
+        $openid=$this->openid($wxcode);
+        $time =date('Y-m-d H:i:s',time());//获取当前时间
+        $dbnum =db('user')->where('openid',$openid)->find();
+        if($dbnum==null){
                 $channel=$data["channel"];
                 $master_id=$data["master_id"];
                 $dbdata = ['id'=>'','openid' =>$openid,'channel' => $channel,'score' => 0,'master_id' => $master_id,'create_time' =>$time ,'updata_time' =>$time];
                 $dbreturn=db('user')->insert($dbdata);
                 return $dbreturn; //返回1
             }
-            else{
+        else{
                  //更新信息
                  $dbreturn= db('user')->where('openid',$openid)->update(['updata_time' => $time]);
                 return $dbreturn;//返回1
             }
-        }
-
+        
     }
 
-     public function test(Request $request)
+
+     //请求获取openid
+    public function obtainopenid(Request $request)
     {
-        $data =$request->param();//接收所有传过来的post值
-        //$data =$request->param("name");//接收所有传过来的post值
-        $name=$data['name'];//获取某个固定值
-         return $name;
-        // $test = test();
-        // return $test;
+        $wxcode =$request->param("code");//接收所有传过来的值
+        $openid=openid($wxcode);
+        return $openid;
+    }
+
+     public function test( Request $request)
+    {
+
+        $token =$request->isPost();
+        return $token;
+        // $data =$request->param();//接收所有传过来的post值
+        // //$data =$request->param("name");//接收所有传过来的post值
+        // $wxcode=$data['code'];//获取某个固定值
+        // $openid=openid($wxcode);
+        // return $openid;
+        // // $test = test();
+        // // return $test;
     }
 }
